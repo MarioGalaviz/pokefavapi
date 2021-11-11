b9db70b07be1c8954b8cce24f10ecd6a67e28050
Mario <mga.375@gmail.com>
2021-11-11 16:39:03 -0600

    intento 47000

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors(
-  {
-    origin : 'http://localhost:3000',
-    credentials: true
-  }
+  // {
+  //   origin : 'http://localhost:3000',
+  //   credentials: true
+  // }
 ))
 app.use(session({ 
   secret: 'juan the keyboard cat',
@@ -26,15 +26,16 @@ app.use(session({
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header(
-//   "Access-Control-Allow-Headers",
-//   "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-//   });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+  "Access-Control-Allow-Headers",
+  "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+  });
 
 
 
