6a09a9c540efdba7f7a25f4a086b4e88306b5793
Mario <mga.375@gmail.com>
2021-11-11 15:37:20 -0600

    agregar local host a header

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "frontendURL");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
   "Access-Control-Allow-Headers",
