d224e309b53c21b04093f82fe3fd6767f961ed6f
Mario <mga.375@gmail.com>
2021-11-11 17:18:06 -0600

    comillas a true

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header(
   "Access-Control-Allow-Headers",
